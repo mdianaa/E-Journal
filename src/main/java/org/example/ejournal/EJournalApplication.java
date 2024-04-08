42937e6e4b92b2803003101b13e4846c7f44c768
mdianaa <d.mihaylova.2002@gmail.com>
2024-04-08 21:21:18 +0300

    first commit

---
+++ src/main/java/org/example/ejournal/EJournalApplication.java
@@ -0,0 +1,13 @@
+package org.example.ejournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EJournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EJournalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ejournal/EJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ejournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EJournalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
