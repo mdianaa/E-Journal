41345cbb1ceecfbffc0365767bf47eacdf8359b5
mdianaa <d.mihaylova.2002@gmail.com>
2024-06-29 17:21:36 +0300

    added cors

--- src/main/java/org/example/ejournal/EJournalApplication.java
+++ src/main/java/org/example/ejournal/EJournalApplication.java
@@ -2,6 +2,9 @@ package org.example.ejournal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EJournalApplication {
--- src/main/java/org/example/ejournal/config/Config.java
+++ src/main/java/org/example/ejournal/config/Config.java
@@ -3,6 +3,8 @@ package org.example.ejournal.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class Config {
@@ -11,5 +13,18 @@ public class Config {
     @Bean
     public ModelMapper createModelMapper() {
         return new ModelMapper();
+    }
+
+    // CORS configuration
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // to match all patterns
+                        .allowedOrigins("http://localhost:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE"); // for these requests
+            }
+        };
     }
 }
