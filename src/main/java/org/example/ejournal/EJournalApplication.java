a460d63f9963904d90621ab9bb7220f39115abef
mdianaa <d.mihaylova.2002@gmail.com>
2026-04-14 14:31:34 +0300

    Enabled scheduling

--- src/main/java/org/example/ejournal/EJournalApplication.java
+++ src/main/java/org/example/ejournal/EJournalApplication.java
@@ -2,11 +2,10 @@ package org.example.ejournal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EJournalApplication {
 
     public static void main(String[] args) {
